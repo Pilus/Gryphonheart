87c638ab5f033283dd6b2c7204461983e26aa2ed
Pilus <pilus@pilus.info>
2014-02-09 19:29:13 +0100

    More progress on BBCode converter.

--- GHI/Tests/UI/Book/Converters/GHI_BBCodeConverter.lua
+++ GHI/Tests/UI/Book/Converters/GHI_BBCodeConverter.lua
@@ -8,16 +8,17 @@ GHTest.AddTest(name.."ShouldImplementTheCorrectInterface", function()
 	GHTest.Equals("function",type(converter.ToSimpleHtml),"It should implement a .ToSimpleHtml function");
 end);
 
+--[[
 GHTest.AddTest(name.."ShouldConvertPlainTextIntoPlainTextInBothWays", function()
 	local converter = GHI_BBCodeConverter();
-	local simpleHtml = "First text < 4 \\5 \\> f";
+	local simpleHtml = "<html><body><p>First text < 4 \\5 \\> f</p></body></html>";
 	local mockup = converter.ToMockup(simpleHtml);
 
-	GHTest.Equals("<html><body><p>First text < 4 \\5 \\> f</p></body></html>", mockup)
+	GHTest.Equals("First text < 4 \\5 \\> f", mockup)
 
 	local simple2 = converter.ToSimpleHtml(mockup);
 	GHTest.Equals(mockup, simple2);
-end);
+end); --]]
 
 GHTest.AddTest(name.."ShouldConvertNestedTags", function()
 	local converter = GHI_BBCodeConverter();
@@ -27,10 +28,23 @@ GHTest.AddTest(name.."ShouldConvertNestedTags", function()
 
 end);
 
-GHTest.AddTest(name.."ShouldConvertHnCorrectlyIncludingAlignment", function()
+GHTest.AddTest(name.."ShouldConvertH1Correctly", function()
 	local converter = GHI_BBCodeConverter();
 
-	GHTest.Equals('<html><body><p>Some</p><h1 align="c">Head line</h1><p>after</p></body></html>',
+	GHTest.Equals('<html><body><p>Some</p><h1>Head line</h1><p>after</p></body></html>',
 		converter.ToSimpleHtml("Some[h1]Head line[/h1]after"));
+end);
+
+GHTest.AddTest(name.."ShouldConvertAlignmentCorrectly", function()
+	local converter = GHI_BBCodeConverter();
+
+	GHTest.Equals('<html><body><p>Before</p><p align="left">To the left</p><p>after</p></body></html>',
+		converter.ToSimpleHtml("Before[left]To the left[/left]after"));
+
+	GHTest.Equals('<html><body><p>Before</p><h1>Head </h1><h1 align="left">To the left</h1><h1>Line</h1><p>after</p></body></html>',
+			converter.ToSimpleHtml("Before[h1]Head [left]To the left[/left]Line[/h1]after"));
+
+	GHTest.Equals('<html><body><p>Before</p><h1 align="left">To the left</h1></h1><p>after</p></body></html>',
+				converter.ToSimpleHtml("Before[h1][left]To the left[/left][/h1]after"));
 end);
 
--- GHI/UI/Book/Converters/GHI_BBCodeConverter.lua
+++ GHI/UI/Book/Converters/GHI_BBCodeConverter.lua
@@ -19,46 +19,114 @@ function GHI_BBCodeConverter()
 	end
 	class = GHClass("GHI_BBCodeConverter");
 
+	local MinimumTags;
+
 	local OPEN_REGEX = "%[([%a%d]+)%]";
 	local CLOSE_REGEX = function(tag) return "%[/"..tag.."%]"; end
+
+
+	local HtmlToTable;
+	HtmlToTable = function(html)
+
+	end
 
 	class.ToMockup = function(simpleHtml)
 		return simpleHtml;
 	end
 
+	local BuildOpenTag = function(meta, first)
+		if meta.tag == "left" or meta.tag == "right" or meta.tag == "center" then
+			local actualTag = meta.parent;
+			if MinimumTags[actualTag] then
+				actualTag = MinimumTags[actualTag];
+				return string.format('<%s align="%s">', actualTag, meta.tag);
+			elseif first then
+				return string.format('<%s align="%s">', actualTag, meta.tag), true;
+			else
+				return string.format('</%s><%s align="%s">', actualTag, actualTag, meta.tag);
+			end
+		end
+		return "<"..meta.tag..">";
+	end
+
+	local BuildCloseTag = function(meta, last)
+		if meta.tag == "left" or meta.tag == "right" or meta.tag == "center" then
+			local actualTag = meta.parent;
+			if MinimumTags[actualTag] then
+				actualTag = MinimumTags[actualTag];
+				return string.format("</%s>", actualTag, actualTag);
+			elseif last then
+				return string.format("</%s>", actualTag, actualTag), true;
+			else
+				return string.format("</%s><%s>", actualTag, actualTag);
+			end
+		end
+		return "</"..meta.tag..">";
+	end
+
 	local ConvertBlocksToText
-	ConvertBlocksToText = function(blocks)
+	ConvertBlocksToText = function(blocks, first, last)
 		if type(blocks) == "string" then
 			return blocks;
 		end
-		local s = "";
+		local openTag, closeTag = "", "";
+		local skipFirst, skipLast;
 		if blocks.meta then
-			s = "<"..blocks.meta.tag..">";
+			openTag, skipFirst = BuildOpenTag(blocks.meta, first);
+			closeTag, skipLast = BuildCloseTag(blocks.meta, last);
 		end
 
+		local body = "";
 		for i=1,#(blocks) do
-			s = s..ConvertBlocksToText(blocks[i]);
+			local blockBody, skip = ConvertBlocksToText(blocks[i], i==1, i==#(blocks));
+			body = body .. blockBody;
+
+			if skip then
+				if i == 1 then
+					openTag = "";
+				elseif i==#(blocks) then
+					closeTag = "";
+				end
+			end
 		end
 
-		if blocks.meta then
-			s = s.."</"..blocks.meta.tag..">";
-		end
-
-		return s;
+		return openTag..body..closeTag,
+		(first and skipFirst) or (last and skipLast);
 	end
 
+	MinimumTags = {
+		body = "p",
+	};
+
 	local ConvertTagsToBlocks;
-	ConvertTagsToBlocks = function(text, i, j, meta)
+	ConvertTagsToBlocks = function(text, i, j, meta, parentTag)
 		local blocks = {
 			meta = meta,
 		};
+		local subMeta;
+		if MinimumTags[parentTag] then
+			subMeta = {
+				tag = MinimumTags[parentTag],
+				parent = parentTag,
+			};
+		end
 		while (i) do
 			local tagOpenStart, tagOpenEnd, tag = string.find(text, OPEN_REGEX, i);
 			if not(tagOpenStart) then
-				table.insert(blocks, string.sub(text, i, j));
+				if i < (j or string.len(text))+1 then
+					table.insert(blocks, {
+						string.sub(text, i, j),
+						meta = subMeta,
+					});
+				end
 				break;
 			else
-				table.insert(blocks, string.sub(text, i, tagOpenStart - 1));
+				if i < tagOpenStart then
+					table.insert(blocks, {
+						string.sub(text, i, tagOpenStart - 1),
+						meta = subMeta,
+					});
+				end
 
 				local tagCloseStart, tagCloseEnd = string.find(text, CLOSE_REGEX(tag), tagOpenEnd);
 
@@ -66,7 +134,10 @@ function GHI_BBCodeConverter()
 					table.insert(blocks, ConvertTagsToBlocks(text, tagOpenEnd, j));
 					break;
 				else
-					table.insert(blocks, ConvertTagsToBlocks(text, tagOpenEnd+1, tagCloseStart-1, {tag = tag, }))
+					table.insert(blocks, ConvertTagsToBlocks(text, tagOpenEnd+1, tagCloseStart-1, {
+						tag = tag,
+						parent = parentTag,
+					}, tag))
 					i = tagCloseEnd+1;
 				end
 			end
@@ -79,9 +150,9 @@ function GHI_BBCodeConverter()
 	end
 
 	class.ToSimpleHtml = function(mockup)
-		local blocks = ConvertTagsToBlocks(mockup, 0)
+		local blocks = ConvertTagsToBlocks(mockup, 0, nil, nil, "body")
 
-		return AppendHtmlAndBodyTags(ConvertBlocksToText(blocks));
+		return AppendHtmlAndBodyTags(ConvertBlocksToText(blocks, true));
 	end
 
 	return class;
