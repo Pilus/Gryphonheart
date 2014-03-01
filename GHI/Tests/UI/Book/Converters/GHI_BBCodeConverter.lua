2923f0403370227412f2376d538c7aab426a56f3
Pilus <pilus@pilus.info>
2014-03-01 23:37:14 +0100

    BBCode converter progress.

--- GHI/Tests/UI/Book/Converters/GHI_BBCodeConverter.lua
+++ GHI/Tests/UI/Book/Converters/GHI_BBCodeConverter.lua
@@ -8,24 +8,32 @@ GHTest.AddTest(name.."ShouldImplementTheCorrectInterface", function()
 	GHTest.Equals("function",type(converter.ToSimpleHtml),"It should implement a .ToSimpleHtml function");
 end);
 
---[[
 GHTest.AddTest(name.."ShouldConvertPlainTextIntoPlainTextInBothWays", function()
 	local converter = GHI_BBCodeConverter();
-	local simpleHtml = "<html><body><p>First text < 4 \\5 \\> f</p></body></html>";
-	local mockup = converter.ToMockup(simpleHtml);
+	local mockup = "First text < 4 \\5 \\> f";
 
-	GHTest.Equals("First text < 4 \\5 \\> f", mockup)
+	local html = converter.ToSimpleHtml(mockup);
+	GHTest.Equals("<html><body><p>First text &lt; 4 \\5 \\&gt; f</p></body></html>", html)
 
-	local simple2 = converter.ToSimpleHtml(mockup);
-	GHTest.Equals(mockup, simple2);
-end); --]]
+	local mockup2 = converter.ToMockup(html);
+	GHTest.Equals(mockup, mockup2);
+end);
 
 GHTest.AddTest(name.."ShouldConvertNestedTags", function()
 	local converter = GHI_BBCodeConverter();
 
-	GHTest.Equals('<html><body><p>t</p><a>x<b>y<c>z</c>æ</b>ø</a><p>å</p></body></html>',
-	converter.ToSimpleHtml("t[a]x[b]y[c]z[/c]æ[/b]ø[/a]å"));
+	GHTest.Equals('<html><body><p>t</p><h1>x</h1><p>å</p></body></html>',
+	converter.ToSimpleHtml("t[h1]x[/h1]å"));
 
+	GHTest.Equals('<html><body><h1>x</h1></body></html>',
+		converter.ToSimpleHtml("[h1]x[/h1]"));
+end);
+
+GHTest.AddTest(name.."ShouldConvertNestedTagsNotBreakingP", function()
+	local converter = GHI_BBCodeConverter();
+
+	GHTest.Equals('<html><body><p>t\124T:0:0:<a>x\124T:0:0:<b>y\124T:0:0:<c>z</c>\124tæ</b>\124tø</a>\124tå</p></body></html>',
+	converter.ToSimpleHtml("t[a]x[b]y[c]z[/c]æ[/b]ø[/a]å"));
 end);
 
 GHTest.AddTest(name.."ShouldConvertH1Correctly", function()
@@ -34,6 +42,7 @@ GHTest.AddTest(name.."ShouldConvertH1Correctly", function()
 	GHTest.Equals('<html><body><p>Some</p><h1>Head line</h1><p>after</p></body></html>',
 		converter.ToSimpleHtml("Some[h1]Head line[/h1]after"));
 end);
+
 
 GHTest.AddTest(name.."ShouldConvertAlignmentCorrectly", function()
 	local converter = GHI_BBCodeConverter();
@@ -44,7 +53,26 @@ GHTest.AddTest(name.."ShouldConvertAlignmentCorrectly", function()
 	GHTest.Equals('<html><body><p>Before</p><h1>Head </h1><h1 align="left">To the left</h1><h1>Line</h1><p>after</p></body></html>',
 			converter.ToSimpleHtml("Before[h1]Head [left]To the left[/left]Line[/h1]after"));
 
-	GHTest.Equals('<html><body><p>Before</p><h1 align="left">To the left</h1></h1><p>after</p></body></html>',
+	GHTest.Equals('<html><body><p>Before</p><h1 align="left">To the left</h1><p>after</p></body></html>',
 				converter.ToSimpleHtml("Before[h1][left]To the left[/left][/h1]after"));
+end);     --]]
+
+
+GHTest.AddTest(name.."ShouldConvertH1Correctly", function()
+	local converter = GHI_BBCodeConverter();
+
+	GHTest.Equals('<html><body><p>Some</p><h1>Head line</h1><p>after</p></body></html>',
+		converter.ToSimpleHtml("Some[h1]Head line[/h1]after"));
 end);
 
+GHTest.AddTest(name.."ShouldConvertArgumentsCorrectly", function()
+	local converter = GHI_BBCodeConverter();
+
+	local bbCode = "Some[obj arg1=A]Text[/obj]after";
+
+	local html = converter.ToSimpleHtml(bbCode);
+	GHTest.Equals('<html><body><p>Some\124T:0:0:<obj arg1="A">Text</obj>\124tafter</p></body></html>', html);
+
+	local bbCode2 = converter.ToMockup(html);
+	GHTest.Equals(bbCode, bbCode2);
+end);
