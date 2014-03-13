eb5e3ce32fcf68062b7d9beaf4c68d351ec25618
Pilus <pilus@pilus.info>
2014-03-13 08:16:44 +0100

    Oncommited stuff part 1

--- GHM/Toolbar/GHM_MultiPageToolbar.lua
+++ GHM/Toolbar/GHM_MultiPageToolbar.lua
@@ -14,66 +14,76 @@ function GHM_MultiPageToolbar(parent, main, profile)
 	local frame = CreateFrame("Frame", "GHM_MultiPageToolbar" .. count, parent);
 	count = count + 1;
 
+	local buttonHeight = 19;
+	local backgroundColor = {0.5, 0.5, 0.5, 0.5};
+	local backgroundColor2 = {0.8, 0.8, 0.8, 0.6};
+
 	local height,width = 0,0;
 	local pages = {};
+	local pageButtons = {};
 
-	local lastClicked;
-	local TogglePage = function(self)
-		if lastClicked then
-			lastClicked:Enable();
-			pages[lastClicked.id]:Hide();
+	local currentShown;
+	local TogglePage = function(i)
+		if currentShown then
+			pages[currentShown]:Hide();
+			pageButtons[currentShown].SetBGTextureColor(unpack(backgroundColor2))
 		end
-		self:Disable();
-		pages[self.id]:Show();
-		lastClicked = self;
+
+		pages[i]:Show();
+		pageButtons[i].SetBGTextureColor(unpack(backgroundColor));
+		currentShown = i;
 	end
 
-	local buttonRowProfile = {};
-	local firstButton
 	for i = 1,#(profile) do
-		table.insert(buttonRowProfile,{
-			type = "Button",
-			text = profile[i].text,
-			align = "l",
-			compact = true,
-			OnClick = function(self)
-				TogglePage(self);
-			end,
-			OnLoad = function(self)
-				self:SetDisabledTexture(self:GetPushedTexture());
-				self.id = i;
-				if i == 1 then
-					firstButton = self;
-				end
-			end,
-		})
-	end
+		local f = CreateFrame("Button", frame:GetName().."PageButton"..i, frame, "GHM_ToolbarPageButtonTemplate");
+		local textFrame = _G[f:GetName().."Text"];
+		textFrame:SetText(profile[i].name);
 
-	local buttonRow = GHM_ToolbarObjectRow(frame, main, buttonRowProfile);
+		f:SetWidth(textFrame:GetWidth() + 16);
+		f:SetHeight(buttonHeight);
+
+		f.SetBGTextureColor = function(r, g, b, a)
+			_G[f:GetName().."Bg"]:SetTexture(r, g, b, a);
+		end
+		f.SetBGTextureColor(unpack(backgroundColor2));
+
+		if pageButtons[i-1] then
+			f:SetPoint("LEFT", pageButtons[i-1], "RIGHT");
+		else
+			f:SetPoint("TOPLEFT", frame, "TOPLEFT");
+		end
+
+		f:SetScript("OnClick",function()
+			TogglePage(i);
+		end);
+
+		pageButtons[i] = f;
+	end
 
 	for i=1,#(profile) do
-		local page = GHM_ToolbarObjectRow(frame, main, profile[i]);
-		page:SetPoint("TOPRIGHT","TOPRIGHT");
-
+		local page = GHM_ToolbarPage(frame, main, profile[i]);
+		page:SetPoint("TOPLEFT", frame, "TOPLEFT", 2, - (buttonHeight + 2));
+		page:Hide();
 		width = math.max(width, page:GetWidth());
 		height = math.max(height, page:GetHeight());
 
 		table.insert(pages, page)
 	end
 
-	local h, text = GHM_Text(frame, main, {
-		text = profile.name,
-		align = "c",
-	})
-	text:ClearAllPoints();
-	text:SetPoint("BOTTOM","BOTTOM");
+	height = height + buttonHeight + 4;
+	width = width + 4;
 
 	frame:SetHeight(height);
 	frame:SetWidth(width);
 
+	local bg = frame:CreateTexture()
+	bg:SetTexture(unpack(backgroundColor));
+	bg:SetPoint("TOPLEFT", frame, "TOPLEFT", 0, -buttonHeight);
+	bg:SetPoint("BOTTOMRIGHT", frame, "BOTTOMRIGHT", 0, 0);
+
 	GHM_FramePositioning(frame,profile,parent);
 
-	TogglePage(firstButton);
+	TogglePage(1);
 
 	return frame;
 end
