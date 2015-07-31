aa849d06f84f45c4cfed4f62187663e34a30f3f1
Pilus <pilus@pilus.info>
2015-07-31 18:04:50 +0200

    Display ui xml reading errors.

--- WoWSimulator/UISimulation/XMLHandler/XmlUiLoader.cs
+++ WoWSimulator/UISimulation/XMLHandler/XmlUiLoader.cs
@@ -1,20 +1,70 @@
 ﻿namespace WoWSimulator.UISimulation.XMLHandler
 {
+    using System;
     using System.IO;
     using System.Xml.Serialization;
+    using System.Xml;
+    using System.Xml.Schema;
 
     public static class XmlUiLoader
     {
+        private static StringWriter stringWriter;
         public static Ui Load(string xmlFilePath)
         {
             var stream = new FileStream(xmlFilePath, FileMode.Open, FileAccess.Read, FileShare.Read);
 
             var serializer = new XmlSerializer(typeof(Ui));
+            serializer.UnknownAttribute += Serializer_UnknownAttribute;
+            serializer.UnknownElement += Serializer_UnknownElement;
+            serializer.UnknownNode += serializer_UnknownNode;
+
+            stringWriter = new StringWriter();
 
             var ui = (Ui)serializer.Deserialize(stream);
             stream.Close();
 
+            var errors = stringWriter.ToString();
+            if (errors.Length > 0)
+            {
+                throw new UiSimuationException("Errors loading xml:\n" + errors);
+            }
+
             return ui;
+        }
+
+        private static void Serializer_UnknownElement(object sender, XmlElementEventArgs e)
+        {
+
+            stringWriter.WriteLine("Unknown Element");
+            stringWriter.WriteLine(e.Element.Name + " " + e.Element.InnerXml);
+            stringWriter.WriteLine("LineNumber: " + e.LineNumber);
+            stringWriter.WriteLine("LinePosition: " + e.LinePosition);
+            stringWriter.WriteLine(sender.ToString());
+            stringWriter.WriteLine("");
+        }
+
+        private static void Serializer_UnknownAttribute(object sender, XmlAttributeEventArgs e)
+        {
+            
+            stringWriter.WriteLine("Unknown Attribute");
+            stringWriter.WriteLine(e.Attr.Name + " " + e.Attr.InnerXml);
+            stringWriter.WriteLine("LineNumber: " + e.LineNumber);
+            stringWriter.WriteLine("LinePosition: " + e.LinePosition);
+            stringWriter.WriteLine(sender.ToString());
+            stringWriter.WriteLine("");
+        }
+
+        private static void serializer_UnknownNode(object sender, XmlNodeEventArgs e)
+        {
+            stringWriter.WriteLine("UnknownNode Name: {0}", e.Name);
+            stringWriter.WriteLine("UnknownNode LocalName: {0}", e.LocalName);
+            stringWriter.WriteLine("UnknownNode Namespace URI: {0}", e.NamespaceURI);
+            stringWriter.WriteLine("UnknownNode Text: {0}", e.Text);
+
+            XmlNodeType myNodeType = e.NodeType;
+            stringWriter.WriteLine("NodeType: {0}", myNodeType);
+
+            stringWriter.WriteLine();
         }
     }
 }
