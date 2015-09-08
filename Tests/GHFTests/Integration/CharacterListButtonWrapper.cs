40af7b7984291d1ed75e360a17cac0132de5983b
Pilus <pilus@pilus.info>
2015-09-08 18:01:47 +0200

    Missing files.

---
+++ Tests/GHFTests/Integration/CharacterListButtonWrapper.cs
@@ -0,0 +1,25 @@
+﻿namespace Tests.GHFTests.Integration
+{
+    using BlizzardApi.WidgetInterfaces;
+    using GHF.View.CharacterMenuProfile.CharacterList;
+    using WoWSimulator.UISimulation;
+    using WoWSimulator.UISimulation.UiObjects;
+    using Wrappers;
+
+    public class CharacterListButtonWrapper : CheckButton, ICharacterListButton
+    {
+        public static IUIObject Init(UiInitUtil util, LayoutFrameType layout, IRegion parent)
+        {
+            return new CharacterListButtonWrapper(util, layout, parent);
+        }
+
+        public CharacterListButtonWrapper(UiInitUtil util, LayoutFrameType layout, IRegion parent)
+            : base(util, "checkButton", layout as CheckButtonType, parent)
+        { }
+
+        public IFontString NameLabel
+        {
+            get { return (IFontString) this["NameLabel"]; }
+        }
+    }
+}
---
+++ Tests/Wrappers/TextObjectFrameWrapper.cs
@@ -0,0 +1,25 @@
+﻿namespace Tests.Wrappers
+{
+    using BlizzardApi.WidgetInterfaces;
+    using GH.Menu.Objects.Text;
+    using WoWSimulator.UISimulation;
+    using WoWSimulator.UISimulation.UiObjects;
+
+    public class TextObjectFrameWrapper : Frame, ITextObjectFrame
+    {
+        public static IUIObject Init(UiInitUtil util, LayoutFrameType layout, IRegion parent)
+        {
+            return new TextObjectFrameWrapper(util, layout, parent);
+        }
+
+        public TextObjectFrameWrapper(UiInitUtil util, LayoutFrameType layout, IRegion parent)
+            : base(util, "frame", layout as FrameType, parent)
+        { }
+
+        public IFontString Label
+        {
+            get { return (IFontString)this["Label"]; }
+        }
+
+    }
+}
